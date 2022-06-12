a85e8dd4786705562a326e0ae4bd9e0f363dbc37
vu-161200 <82396199+vu-161200@users.noreply.github.com>
2022-06-12 22:41:58 +0700

    Add files via upload

---
+++ ChatClient/Resource.h
@@ -0,0 +1,43 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by ChatClient.rc
+
+#define IDS_APP_TITLE			103
+
+#define IDR_MAINFRAME			128
+#define IDD_CHATCLIENT_DIALOG	102
+#define IDD_ABOUTBOX			103
+#define IDM_ABOUT				104
+#define IDM_EXIT				105
+#define IDI_CHATCLIENT			107
+#define IDI_SMALL				108
+#define IDC_CHATCLIENT			109
+#define IDC_LIST_MESSAGE                110
+#define IDC_LIST_CLIENT                 111
+#define IDC_EDIT_MESSAGE                112
+#define IDC_EDIT_MESSAGE_PRIVATE        112
+#define IDC_BUTTON_SEND                 113
+#define IDR_MAINFRAME                   128
+#define IDD_DIALOG_LOGIN                129
+#define IDD_DIALOG_CONNECT              129
+#define IDD_DIALOG1                     130
+#define IDD_DIALOG_CHAT                 131
+#define ID_FILE_CONNECT                 32773
+#define ID_FILE_DISCONNECT              32774
+#define IDC_EDIT_CLIENT_ID              1000
+#define IDC_MYICON				2
+#ifndef IDC_STATIC
+#define IDC_STATIC				-1
+#endif
+// Next default values for new objects
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+
+#define _APS_NO_MFC					130
+#define _APS_NEXT_RESOURCE_VALUE	129
+#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_CONTROL_VALUE		1000
+#define _APS_NEXT_SYMED_VALUE		110
+#endif
+#endif
